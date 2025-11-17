e1920ac1f850cc408808c6496a76289072942191
Rustam <77282026+Rdast512@users.noreply.github.com>
2025-11-17 19:33:46 +0300

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,17 +1,18 @@
-#include <iostream>
+//
+// Created by rdast on 21.08.2025.
+//
+
+#include "Application.h"
+
 
 int main() {
-#ifdef _LIBCPP_VERSION
-    std::cout << "Using libc++ (LLVM/Clang standard library)\n";
-    std::cout << "Version: " << _LIBCPP_VERSION << "\n";
-#elif defined(__GLIBCXX__)
-    std::cout << "Using libstdc++ (GNU standard library)\n";
-    std::cout << "Version: " << __GLIBCXX__ << "\n";
-#elif defined(_MSVC_STL_VERSION)
-    std::cout << "Using MSVC STL\n";
-    std::cout << "Version: " << _MSVC_STL_VERSION << "\n";
-#else
-    std::cout << "Unknown standard library\n";
-#endif
-    return 0;
+    try {
+        Application app;
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
