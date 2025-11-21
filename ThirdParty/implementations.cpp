abb4e5b5ce1b7b7f7382a4cc71b85449ef53cc4b
Rustam <77282026+Rdast512@users.noreply.github.com>
2025-11-21 12:59:09 +0300

    linux fix

--- ThirdParty/implementations.cpp
+++ ThirdParty/implementations.cpp
@@ -2,5 +2,6 @@
 #include "../ThirdParty/vk_mem_alloc.h"
 
 #define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "../ThirdParty/tiny_obj_loader.h"
--- src/core/main.cpp
+++ src/core/main.cpp
@@ -3,9 +3,38 @@
 //
 
 #include "Application.h"
+#include <iostream>
 
+
+void CheckSTL() {
+    std::cout << "--------------------------------------------------\n";
+    std::cout << "Build Configuration Check:\n";
+
+    // 1. Check Compiler
+    #if defined(__clang__)
+        std::cout << "  Compiler: Clang " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__ << "\n";
+    #elif defined(__GNUC__)
+        std::cout << "  Compiler: GCC " << __GNUC__ << "." << __GNUC_MINOR__ << "\n";
+    #elif defined(_MSC_VER)
+        std::cout << "  Compiler: MSVC " << _MSC_VER << "\n";
+    #endif
+
+    // 2. Check Standard Library (STL)
+    #if defined(_LIBCPP_VERSION)
+        std::cout << "  STL:      LLVM libc++ (Version: " << _LIBCPP_VERSION << ")\n";
+    #elif defined(__GLIBCXX__)
+        std::cout << "  STL:      GNU libstdc++ (Date: " << __GLIBCXX__ << ")\n";
+    #elif defined(_MSVC_STL_VERSION)
+        std::cout << "  STL:      Microsoft STL (Version: " << _MSVC_STL_VERSION << ")\n";
+    #else
+        std::cout << "  STL:      Unknown\n";
+    #endif
+
+    std::cout << "--------------------------------------------------\n";
+}
 
 int main() {
+    CheckSTL();
     try {
         Application app;
         app.run();
